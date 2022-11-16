2bef8e813a9bb8a8828e5b82a746440e7af2f8e5
Aswathi Suseelan <aswathi.suseelan@AD.MSYSTECHNOLOGIES.COM>
2022-11-16 18:07:45 +0530

    added latest commit activities mfe

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card p-5 mt-4">
+      <h3 className="mb-4">Weekly Commit</h3>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
